ca9da12ae5ef30a033b5189d3ad7a88177516442
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-10-28 01:00:57 -0200

    Reorganized code structure

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ local Fruit = require 'Fruit'
 local w, h = 800, 600
 local debug, mute, pause, inic, death = false, false, false, false, false
 local snake, fruit, field, sfx = {}, {}, {}
-local inicial_size = 4
+local initial_size = 4
 local v = 0.05
 
 sfx = {
@@ -13,9 +13,7 @@ sfx = {
    inicio = love.audio.newSource("/sfx/inicio.ogg", "static"),
 }
 
-local a, b = 0, 0
-
-love.window.setTitle("Snake LOVE2D")
+love.window.setTitle("Snakey")
 
 local function start(first_time)
 
@@ -30,7 +28,7 @@ local function start(first_time)
    -- sets snake and fruit
    snake = Snake.new(w/20, h/20)
    fruit = Fruit.new(love.math.random(2, (w/10)-1), love.math.random(2, (h/10)-1))
-   for i=2, inicial_size do
+   for i=2, initial_size do
       snake:add_segment()
    end
    for _, segment in ipairs(snake:get_segments()) do
@@ -40,6 +38,68 @@ local function start(first_time)
    -- plays start tune
    if not mute and not first_time then
       sfx.inicio:play()
+   end
+end
+
+local function move_snake(v)
+   local function follow_segment(index, direction)
+      if index <= #snake:get_segments() then
+         local segment = snake:get_segments()[index]
+         if segment.direction == 'up' then
+            snake:set_Y(index, snake:get_Y(index)-1)
+         elseif segment.direction == 'down' then
+            snake:set_Y(index, snake:get_Y(index)+1)
+         elseif segment.direction == 'left' then
+            snake:set_X(index, snake:get_X(index)-1)
+         elseif segment.direction == 'right' then
+            snake:set_X(index, snake:get_X(index)+1)
+         end
+         field[segment.x][segment.y] = true
+         follow_segment(index+1, snake:get_direction(index))
+         if index > 1 then
+            snake:set_direction(index, direction)
+         end
+      end
+   end
+   follow_segment(1, snake:get_direction(1))
+   love.timer.sleep(v)
+end
+
+local function apply_effect(fruit)
+   local effect = fruit:get_effect()
+   if effect == 1 then
+      return -- normal fruit, has no special effect
+   elseif effect == 2 then
+      -- adds several fake fruits
+   elseif effect == 3 then
+      -- hurts/cuts snake
+   elseif effect == 4 then
+      -- mirrors controls
+   elseif effect == 5 then
+      -- turns field
+   elseif effect == 6 then
+      -- adds fake/mimic snake
+   elseif effect == 7 then
+      -- speeds snake up
+   elseif effect == 8 then
+      -- makes snake walk backwards
+   elseif effect == 9 then
+      -- empty
+   elseif effect == 10 then
+      -- empty
+   end
+end
+
+local function reset_fruit()
+   fruit:set_X(love.math.random(2, (w/10)-1))
+   fruit:set_Y(love.math.random(2, (h/10)-1))
+end
+
+local function die()
+   death = true
+   inic = false
+   if not mute then
+      sfx.toque:play()
    end
 end
 
@@ -99,67 +159,7 @@ function love.keypressed(key)
    end
 end
 
-local function move_snake(v)
-   local function follow_segment(index, direction)
-      if index <= #snake:get_segments() then
-         local segment = snake:get_segments()[index]
-         if segment.direction == 'up' then
-            snake:set_Y(index, snake:get_Y(index)-1)
-         elseif segment.direction == 'down' then
-            snake:set_Y(index, snake:get_Y(index)+1)
-         elseif segment.direction == 'left' then
-            snake:set_X(index, snake:get_X(index)-1)
-         elseif segment.direction == 'right' then
-            snake:set_X(index, snake:get_X(index)+1)
-         end
-         field[segment.x][segment.y] = true
-         follow_segment(index+1, snake:get_direction(index))
-         if index > 1 then
-            snake:set_direction(index, direction)
-         end
-      end
-   end
-   follow_segment(1, snake:get_direction(1))
-   love.timer.sleep(v)
-end
-
-local function apply_effect(fruit)
-   local effect = fruit:get_effect()
-   if effect == 1 then
-      return -- normal fruit, has no special effect
-   elseif effect == 2 then
-      -- adds several fake fruits
-   elseif effect == 3 then
-      -- hurts/cuts snake
-   elseif effect == 4 then
-      -- mirrors controls
-   elseif effect == 5 then
-      -- turns field
-   elseif effect == 6 then
-      -- adds fake/mimic snake
-   elseif effect == 7 then
-      -- speeds snake up
-   elseif effect == 8 then
-      -- makes snake walk backwards
-   elseif effect == 9 then
-      -- empty
-   elseif effect == 10 then
-      -- empty
-   end
-end
-
 function love.update(dt)
-   local function reset_fruit()
-      fruit:set_X(love.math.random(2, (w/10)-1))
-      fruit:set_Y(love.math.random(2, (h/10)-1))
-   end
-   local function die()
-      death = true
-      inic = false
-      if not mute then
-         sfx.toque:play()
-      end
-   end
    if inic then
       if not pause and not death then
          for i=1, w/10 do
