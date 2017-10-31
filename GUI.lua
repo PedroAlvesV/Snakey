1ccbc1ed07a07d82cb5a4a738163d8ed0e2064f4
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-10-31 21:06:02 -0200

    Started rebasing screens on GUI module
    It will be handled abstractly AbsTK-like.

--- GUI.lua
+++ GUI.lua
@@ -26,7 +26,44 @@ function GUI.random_color()
    return colors[keyset[love.math.random(#keyset)]]
 end
 
-function GUI.logo_screen(w, h)
+function GUI.draw_HUD(w, h, snake, hud_height)
+   -- TODO
+end
+
+function GUI.draw_field(w, h, field, sqr_size)
+   for i=1, w/sqr_size do
+      for j=1, h/sqr_size do
+         if field[i][j] then
+            --love.graphics.setColor(unpack(GUI.random_color()))
+            love.graphics.rectangle("fill", i*sqr_size-sqr_size, j*sqr_size-sqr_size, sqr_size, sqr_size)
+         end
+      end
+   end
+end
+
+function GUI.create_main_screen(w, h)
+   local main_screen
+   -- TODO
+   GUI.main_screen = main_screen
+   return GUI.main_screen
+end
+
+function GUI.create_death_screen(w, h)
+   local death_screen
+   -- TODO
+   GUI.death_screen = death_screen
+   return GUI.death_screen
+end
+
+function GUI.create_pause_screen(w, h)
+   local pause_screen
+   -- TODO
+   GUI.pause_screen = pause_screen
+   return GUI.pause_screen
+end
+
+function GUI.main_screen(w, h)
+   -- must merely call main_screen:draw()
    love.graphics.rectangle("line", w/2-195, h/2-57, 381, 115)
    love.graphics.rectangle("line", w/2-195, h/2-57, 381, 115)
    love.graphics.setColor(unpack(colors.BLACK))
@@ -43,18 +80,8 @@ function GUI.logo_screen(w, h)
    love.graphics.print("Press Space to Start", w/2-108, h/2+59)
 end
 
-function GUI.draw_field(w, h, field, sqr_size)
-   for i=1, w/sqr_size do
-      for j=1, h/sqr_size do
-         if field[i][j] then
-            --love.graphics.setColor(unpack(GUI.random_color()))
-            love.graphics.rectangle("fill", i*sqr_size-sqr_size, j*sqr_size-sqr_size, sqr_size, sqr_size)
-         end
-      end
-   end
-end
-
 function GUI.death_screen(w, h, score)
+   -- must merely call death_screen:draw()
    love.graphics.rectangle("line", w/2-305, h/2-57, 620, 115)
    love.graphics.rectangle("line", w/2-305, h/2-57, 620, 115)
    love.graphics.setColor(unpack(colors.BLACK))
@@ -72,6 +99,7 @@ function GUI.death_screen(w, h, score)
 end
 
 function GUI.pause_screen(w, h)
+   -- must merely call pause_screen:draw()
    love.graphics.rectangle("line", w/2-195, h/2-57, 381, 115)
    love.graphics.rectangle("line", w/2-195, h/2-57, 381, 115)
    love.graphics.setColor(unpack(colors.BLACK))
