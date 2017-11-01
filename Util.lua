86dbac944e47a36b3ca5ea4b07b73236eba651e1
PedroAlvesV <pedroalvesvalentim@gmail.com>
2017-11-01 21:57:58 -0200

    Created Util module to handle some global stuff

---
+++ Util.lua
@@ -0,0 +1,20 @@
+local Util = {}
+
+Util.colors = {
+   BLACK    = {0,0,0},
+   WHITE    = {255,255,255},
+   GRAY     = {128,128,128},
+   RED      = {255,0,0},
+   ORANGE   = {255,128,0},
+   YELLOW   = {255,255,0},
+   LIME     = {128,255,0},
+   GREEN    = {0,255,0},
+   CYAN     = {0,255,255},
+   LBLUE    = {0,128,255},
+   DBLUE    = {0,0,255},
+   PURPLE   = {127,0,255},
+   MAGENTA  = {255,0,255},
+   PINK     = {255,0,127},
+}
+
+return Util
